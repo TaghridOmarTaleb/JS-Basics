85688db150a7bb8ce90d1b7c085070a50a4eb681
taghrid <taghridtaleb1@gmail.com>
2022-12-07 14:07:21 +0200

    Step_1

---
+++ javascript-basics-01/Step_1.js
@@ -0,0 +1,4 @@
+
+
+let nom="name";
+alert(nom);
---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,7 @@
+
+
+let name="Nom : Doe";
+let surname="Prénom : John" ;
+let city="ville : New-York";
+
+alert( "name" + name +"\n"+ surname+"\n"+ city);
