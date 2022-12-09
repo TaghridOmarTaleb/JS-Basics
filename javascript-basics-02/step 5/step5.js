48cd9f2c055c6ca228ccc3cdca1dcbc3b1e1fee8
taghrid <taghridtaleb1@gmail.com>
2022-12-09 12:26:08 +0200

    bas2 step5

---
+++ javascript-basics-02/step 5/step5.js
@@ -0,0 +1,14 @@
+let image1 = document.querySelector("#image1");
+let image2 = document.querySelector("#image2");
+let image3 = document.querySelector("#image3");
+let image4 = document.querySelector("#image4");
+let image5 = document.querySelector("#image5");
+
+let path1 = "images/image1_2.jpg";
+let path2 = "images/image2_2.jpg";
+let path3 = "images/image3_2.jpg";
+let path4 = "images/image4_2.jpg";
+let path5 = "images/image5_2.jpg";
+
+function changeImg(image, path) {
+  image.src = path;}
