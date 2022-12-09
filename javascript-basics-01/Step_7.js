4a120bb2ce8ba5d69e5fab79c3d85cd2454d6d03
taghrid <taghridtaleb1@gmail.com>
2022-12-09 13:05:57 +0200

    bas1 step7

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,8 @@
+
+function calc(num1, num2) {
+    var num1= parseInt(document.getElementById("shoe_size").value);
+    var num2= parseInt(document.getElementById("year").value);
+    var firstTotal = (num1 * 2 + 5) * 50 - num2;
+  
+    alert(Total + 1766);
+  }
