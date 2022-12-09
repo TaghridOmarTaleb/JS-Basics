ea7626baacee9f2077fcc0d60a75816dbfd723a2
taghrid <taghridtaleb1@gmail.com>
2022-12-09 13:04:43 +0200

    bas1 step 6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,5 @@
+function divide(num1, num2) {
+    let num1 = parseInt(document.getElementById("first_number").value);
+    let num2 = parseInt(document.getElementById("second_number").value);
+    return alert(num1%num2);
+  }
