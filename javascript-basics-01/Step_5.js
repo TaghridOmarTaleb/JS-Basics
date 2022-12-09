0cc09297a84529d27414012c2e245488a0b307dd
taghrid <taghridtaleb1@gmail.com>
2022-12-09 13:03:08 +0200

    bas1 step5

--- javascript-basics-01/Step_5.js
+++ javascript-basics-01/Step_5.js
@@ -1,2 +1,9 @@
 
 
+alert("enter 2 numbers to multiply");
+
+function multiply(num1,num2) {
+  var num1 = document.getElementById("first_number").value;
+  var num2= document.getElementById("second_number").value;
+  return alert(num1 * num2);
+}
